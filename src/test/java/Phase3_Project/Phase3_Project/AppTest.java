0cd765ab510110cd2604b648f42caa06dd6944c6
Tintu <tintulejosh@gmail.com>
2021-11-24 00:30:44 +0800

    Phase3

---
+++ src/test/java/Phase3_Project/Phase3_Project/AppTest.java
@@ -0,0 +1,20 @@
+package Phase3_Project.Phase3_Project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ src/test/java/Phase3_Project/Phase3_Project/Test1.java
@@ -0,0 +1,12 @@
+package Phase3_Project.Phase3_Project;
+
+import org.junit.Test;
+
+public class Test1 {
+	@Test
+	public void sampleTest1() {
+	
+	System.out.println("Running JUnit Sample Test1");
+		
+	}
+}
